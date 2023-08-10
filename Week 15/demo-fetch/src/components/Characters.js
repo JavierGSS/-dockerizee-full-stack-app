e70d3f7cda3468f2ea281f89819de2eb738096ba
baba <jagasal@icloud.com>
2023-08-09 23:29:56 -0600

    minor changes

--- Week 15/demo-fetch/src/components/Characters.js
+++ Week 15/demo-fetch/src/components/Characters.js
@@ -6,7 +6,7 @@ const Characters = ({characters = []}) => {
             {characters.map((item, index) => (
                     <div key={index} className="col mb-3">
                         <div className="card" style={{minWidth:"200px"}}>
-                            <img src={item.image} alt=""/>
+                            <img src={item.image} alt={item.name}/>
                             <div className="card-body">
                                 <h5 className="card-title">{item.name}</h5>
                                 <hr />
