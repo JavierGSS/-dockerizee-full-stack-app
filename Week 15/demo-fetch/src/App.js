c781a0cbf42b7c558a06ee09b704e2b3fd3fb7c4
baba <jagasal@icloud.com>
2023-08-10 01:14:09 -0600

    new old fetch

--- Week 15/demo-fetch/src/App.js
+++ Week 15/demo-fetch/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Navbar from "./components/Navbar";
 import Characters from './components/Characters';
 import Pagination from './components/Pagination';
-import axios from 'axios';
 
 
 function App() {
@@ -11,14 +10,7 @@ function App() {
 
   let initialUrl = "https://rickandmortyapi.com/api/character";
 
-  const fetchCharacters = async (url) => {
-    const datos = await axios(url);
-    setInfo(datos.data.info);
-    setCharacters(datos.data.results);
-  }
-
-  // Alternative way with fetch():
-  /*const fetchCharacters = (url) => {
+  const fetchCharacters = (url) => {
     fetch(url)
       .then(response => response.json())
       .then((data) => {
@@ -26,7 +18,7 @@ function App() {
         setInfo(data.info);
       })
       .catch(error => console.log(error));
-  };*/
+  };
 
   const onPrevious = () => {
     fetchCharacters(info.prev);
@@ -38,7 +30,7 @@ function App() {
 
   useEffect(() => {
     fetchCharacters(initialUrl);
-  });
+  }, []);
 
   return (
     <>
